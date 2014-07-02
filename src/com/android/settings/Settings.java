46f9776453e01771a775fdb18175be9894ec396b
Ivan <ivanthegreat15@gmail.com>
2014-07-02 18:05:16 -0400

    Merge CM

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -54,7 +54,6 @@ import android.os.UserManager;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.support.v4.view.MenuItemCompat;
 import android.text.TextUtils;
 import android.telephony.MSimTelephonyManager;
 import android.util.Log;
@@ -160,7 +159,7 @@ public class Settings extends PreferenceActivity
     private static final String VOICE_WAKEUP_PACKAGE_NAME = "com.cyanogenmod.voicewakeup";
     
     public static final String NOTIFIER_EXTRA = "notifier";
-
+    
     static final int DIALOG_ONLY_ONE_HOME = 1;
 
     private static boolean sShowNoHomeNotice = false;
@@ -247,9 +246,8 @@ public class Settings extends PreferenceActivity
         final InputMethodManager imm =
                 (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        mSearchBar = (SettingsAutoCompleteTextView) MenuItemCompat.getActionView(mSearchItem);
-
-        MenuItemCompat.setOnActionExpandListener(mSearchItem, new MenuItemCompat.OnActionExpandListener() {
+        mSearchBar = (SettingsAutoCompleteTextView) mSearchItem.getActionView();
+        mSearchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionCollapse(MenuItem item) {
                 mSearchBar.clearFocus();
@@ -282,8 +280,7 @@ public class Settings extends PreferenceActivity
             AsyncTask<Void, Void, ArrayList<SearchInfo>> {
         @Override
         protected ArrayList<SearchInfo> doInBackground(Void... param) {
-            final ArrayList<SearchInfo> infos = SearchPopulator.getTitles(Settings.this);
-            return infos;
+            return SearchPopulator.loadSearchData(Settings.this);
         }
 
         @Override
@@ -311,11 +308,7 @@ public class Settings extends PreferenceActivity
             getWindow().setUiOptions(getIntent().getIntExtra(EXTRA_UI_OPTIONS, 0));
         }
 
-        Intent i = new Intent(this, SearchPopulator.class);
-        i.putExtra(NOTIFIER_EXTRA, new SearchNotifier(new Handler()));
-        startService(i);
-        mActionBar = getActionBar();
-        mActionBar.setDisplayShowCustomEnabled(true);
+        startPopulatingSearchData();
 
         mAuthenticatorHelper = new AuthenticatorHelper();
         mAuthenticatorHelper.updateAuthDescriptions(this);
@@ -362,6 +355,9 @@ public class Settings extends PreferenceActivity
             getActionBar().setDisplayHomeAsUpEnabled(false);
             getActionBar().setHomeButtonEnabled(false);
         }
+
+        mActionBar = getActionBar();
+        mActionBar.setDisplayShowCustomEnabled(true);
     }
 
     @Override
@@ -400,11 +396,9 @@ public class Settings extends PreferenceActivity
 
     @Override
     public void onBackPressed() {
-        if (mSearchBar != null) {
-            if (mSearchBar.hasFocus()) {
-                mSearchBar.clearFocus();
-                return;
-            }
+        if (mSearchBar != null && mSearchBar.hasFocus()) {
+            mSearchBar.clearFocus();
+            return;
         }
         super.onBackPressed();
     }
@@ -433,6 +427,7 @@ public class Settings extends PreferenceActivity
         mDevelopmentPreferencesListener = null;
     }
 
+    @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mSearchBar.clearFocus();
@@ -586,6 +581,12 @@ public class Settings extends PreferenceActivity
                 }
             }
         }
+    }
+
+    private void startPopulatingSearchData() {
+        Intent i = new Intent(this, SearchPopulator.class);
+        i.putExtra(SearchPopulator.EXTRA_NOTIFIER, new SearchNotifier(new Handler()));
+        startService(i);
     }
 
     @Override
@@ -966,8 +967,7 @@ public class Settings extends PreferenceActivity
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent,
-                            View view, int position, long l) {
+    public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
         SearchInfo info = (SearchInfo) parent.getItemAtPosition(position);
         mSearchBar.setText("");
         mSearchBar.clearFocus();
--- src/com/android/settings/cyanogenmod/ButtonSettings.java
+++ src/com/android/settings/cyanogenmod/ButtonSettings.java
@@ -301,7 +301,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
 
         return false;
     }
-
+    
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         if (preference == mSwapVolumeButtons) {
